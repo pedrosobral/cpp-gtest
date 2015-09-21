093120d9ab78c8ab356531025d92a768416d8696
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-09-21 11:40:38 -0700

    Added definition nearestToZero method

--- randoTest.cpp
+++ randoTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include "rando.h"
 
-/*  
+/*
 	FUNCTIONS WE NEED TO TEST:
 
 	bool shouldWorry(bool,bool,bool);
@@ -13,18 +13,36 @@
 	bool isPrime(int);
 	int nearestToZero(int,int);
 */
- 
+
 class RandoTest : public ::testing::Test
 {
 	protected:
 		RandoTest(){} //constructor runs before each test
 		virtual ~RandoTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(RandoTest, allChildrenSmile)
 {
 	Rando rando;
 	ASSERT_TRUE( rando.shouldWorry(true,true,true) );
+}
+
+TEST(nearestToZero, firstClosest)
+{
+    Rando rando;
+
+    for (size_t i = 1; i < 10; i++) {
+        EXPECT_EQ(i, rando.nearestToZero(i, i+1));
+    }
+}
+
+TEST(nearestToZero, secondClosest)
+{
+    Rando rando;
+
+    for (size_t i = 1; i < 10; i++) {
+        EXPECT_EQ(i, rando.nearestToZero(i+1, i));
+    }
 }
